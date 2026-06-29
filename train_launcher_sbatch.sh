6af6c6473165ba5a9ca23bd8c1eff7aabfaf4c43
alex.schein <alex.schein@noaa.gov>
2026-06-29 22:36:32 +0000

    Fixed python environment loading

--- train_launcher_sbatch.sh
+++ train_launcher_sbatch.sh
@@ -6,14 +6,14 @@
 #SBATCH -o training_runs/%j/log_%j.out
 #SBATCH -e training_runs/%j/log_%j.err
 
-#SBATCH --nodes=2
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1          # BACK TO: one launcher task per node
 #SBATCH --cpus-per-task=24
 #SBATCH --gres=gpu:2                 # 2 GPUs per node
 #SBATCH --mem=0
 # NO --gpus-per-task - let all GPUs be visible to the launcher task
 
-#SBATCH -t 02:00:00 #12:00:00 #01:30:00
+#SBATCH -t 19:30:00 #01:30:00
 #SBATCH --export=ALL
 
 echo "Starting job"
@@ -25,9 +25,6 @@ export MKL_NUM_THREADS=2
 # --- NCCL / rendezvous ---
 #export NCCL_DEBUG=INFO
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
-
-# Network (tweak iface if needed, e.g., ib0, enp175s0f0np0)
-# export NCCL_SOCKET_IFNAME=^lo,docker0
 
 # Rendezvous (shared by all nodes)
 export MASTER_ADDR=$(scontrol show hostnames "$SLURM_NODELIST" | head -n 1)
@@ -55,9 +52,9 @@ module load python
 echo 'Modules loaded'
 
 source /scratch3/BMC/wrfruc/aschein/miniconda/etc/profile.d/conda.sh
-conda activate ADAF_environment
+# conda activate ADAF_environment
 
-echo "After Python load: CUDA_VISIBLE_DEVICES = $CUDA_VISIBLE_DEVICES"
+# echo "After Python load: CUDA_VISIBLE_DEVICES = $CUDA_VISIBLE_DEVICES"
 
 ###############
 
@@ -70,10 +67,9 @@ srun --ntasks-per-node=2 --mpi=none \
      bash -lc 'echo "Host: $(hostname)"; echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"; nvidia-smi -L || true'
 
 # --- Launch: one torchrun per node; each spawns 2 ranks (1 per GPU) ---
-
 srun --ntasks-per-node=1 --mpi=none \
      --gres=gpu:2 \
-    python -m torch.distributed.run \
+    /scratch3/BMC/wrfruc/aschein/miniconda/envs/ADAF_environment/bin/python -m torch.distributed.run \
     --nnodes="${NNODES}" \
     --nproc_per_node=2 \
     --node_rank="${NODE_RANK}" \
@@ -82,8 +78,8 @@ srun --ntasks-per-node=1 --mpi=none \
     --rdzv_id="${RDZV_ID}" \
      /scratch3/BMC/wrfruc/aschein/ADAF_RTMA/train.py \
      --config_filepath "./config/params_default.yaml" \
-     --max_epochs 3 \
-     --valid_frequency 100 \
+     --max_epochs 200 \
+     --valid_frequency 5 \
      --checkpoint_path "${CHECKPOINT_DIR}/ckpt.tar" \
      --best_checkpoint_path "${CHECKPOINT_DIR}/best_ckpt.tar"
 
